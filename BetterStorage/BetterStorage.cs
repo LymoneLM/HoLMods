5de901bb982883effc3e7c7954908fc1431d118e
LymoneLM <chengziorange233@qq.com>
2025-09-07 17:08:02 +0800

    init:初始提交

---
+++ BetterStorage/BetterStorage.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using HarmonyLib;
+using BepInEx.Configuration;
+
+namespace BetterStorage
+{
+    [BepInPlugin("cc.lymone.HoL.BetterStorage", "BetterStorage", "1.0.0")]
+    public class BetterStorage : BaseUnityPlugin
+    {
+        private static ConfigEntry<int> KuFangConfig;
+        private static ConfigEntry<int> HorseRoomConfig;
+
+        private void Start()
+        {
+            KuFangConfig = Config.Bind<int>("Config", "KuFang_Multiplier", 100, "库房库存倍率");
+            HorseRoomConfig = Config.Bind<int>("Config", "HorseRoom_Multiplier", 10, "马厩大小倍率");
+            Harmony.CreateAndPatchAll(typeof(BetterStorage));
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(FormulaData), "CunNum_KuFang")]
+        public static bool multi_kufang_size(ref int Lv)
+        {
+            Lv = Lv * KuFangConfig.Value;
+            return true;
+        }
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(FormulaData), "MaNum_HorseRoom")]
+        public static bool multi_horseroom_size(ref int Lv)
+        {
+            Lv = Lv * HorseRoomConfig.Value;
+            return true;
+        }
+    }
+}
---
+++ TooManyStudent/TooManyStudent.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System.Collections.Generic;
+
+namespace TooManyStudent
+{
+    [BepInPlugin("cc.lymone.HoL.TooManyStudent", "TooManyStudent", "1.0.0")]
+    public class TooManyStudent : BaseUnityPlugin
+    {
+        private static ConfigEntry<int> TeacherConfig;
+        private static ConfigEntry<int> StudentConfig;
+
+        private void Start()
+        {
+            TeacherConfig = Config.Bind<int>("Config", "Teacher_Multiplier", 2, "教师数量乘数");
+            StudentConfig = Config.Bind<int>("Config", "Student_Multiplier", 2, "学生容量乘数");
+            Harmony.CreateAndPatchAll(typeof(TooManyStudent));
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(FormulaData), "SiShuTSnum")]
+        public static void Multi_TSnum(ref List<int> __result)
+        {
+            __result[0] *= TeacherConfig.Value;
+            __result[1] *= StudentConfig.Value;
+        }
+    }
+}
