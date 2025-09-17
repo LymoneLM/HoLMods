59fcd43178d3fac3e6f0b0ef2d6dba9ac0f20c8f
LymoneLM <chengziorange233@qq.com>
2025-09-17 23:56:59 +0800

    feat(MR): 新增更多分辨率模组

---
+++ MoreResolution/MoreResolution.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+namespace MoreResolution
+{
+    [BepInPlugin("cc.lymone.HoL.MoreResolution", "MoreResolution","1.0.0")]
+    public class MoreResolution : BaseUnityPlugin
+    {
+        private static void Start() {
+            Mainload.AllFenBData.Add(new List<int> { 
+                1600,
+                900
+            });
+        }
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(SetPanel),"Awake")]]
+        public static void CreateFenB(SetPanel __instance) {
+            __instance.transform.Find("FenB").Find("AllClass").GetComponent<Dropdown>().options = new List<Dropdown.OptionData>();
+            for (int i = 0; i < Mainload.AllFenBData.Count; i++) {
+                Dropdown.OptionData optionData = new Dropdown.OptionData();
+                optionData.text = 
+                __instance.transform.Find("FenB").Find("AllClass").GetComponent<Dropdown>().options.Add(optionData);
+                //if (__instance.isChangeLanguage) {
+                //    __instance.transform.Find("LoadSpeed").Find("AllClass").GetComponent<Dropdown>().value = -1;
+                //}
+            }
+        }
+
+    }
+}
