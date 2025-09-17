fdce1e1c02cb30c616d2383d5a52eb9c949163b3
LymoneLM <chengziorange233@qq.com>
2025-09-18 03:01:36 +0800

    feat(MR): 新增分辨率列表和自定义分辨率

--- MoreResolution/MoreResolution.cs
+++ MoreResolution/MoreResolution.cs
@@ -1,31 +1,78 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
+using System;
 using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
+using static UnityEngine.UI.Dropdown;
 
-namespace MoreResolution
-{
-    [BepInPlugin("cc.lymone.HoL.MoreResolution", "MoreResolution","1.0.0")]
-    public class MoreResolution : BaseUnityPlugin
-    {
-        private static void Start() {
-            Mainload.AllFenBData.Add(new List<int> { 
-                1600,
-                900
-            });
+namespace MoreResolution {
+    [BepInPlugin("cc.lymone.HoL.MoreResolution", "MoreResolution", "1.0.0")]
+    public class MoreResolution : BaseUnityPlugin {
+        private static readonly List<List<int>> resolutions = new List<List<int>> {
+            new List<int> { 640, 480 },     // 4:3
+            new List<int> { 800, 600 },     // 4:3
+            new List<int> { 1280, 720 },    // 16:9
+            new List<int> { 1280, 800 },    // 16:10
+            new List<int> { 1280, 960 },    // 4:3
+            new List<int> { 1400, 1050 },   // 4:3
+            new List<int> { 1440, 900 },    // 16:10
+            new List<int> { 1600, 900 },    // 16:9
+            new List<int> { 1600, 1200 },   // 4:3
+            new List<int> { 1680, 1050 },   // 16:10
+            new List<int> { 1920, 1080 },   // 16:9 (Full HD)
+            new List<int> { 1920, 1200 },   // 16:10
+            new List<int> { 2560, 1440 },   // 16:9 (QHD)
+            new List<int> { 2560, 1600 },   // 16:10 (WQXGA)
+            new List<int> { 2880, 1800 },   // 16:10
+            new List<int> { 3200, 1800 },   // 16:9
+            new List<int> { 3440, 1440 },   // 21:9 (超宽屏)
+            new List<int> { 3840, 2160 },   // 16:9 (4K UHD)
+            new List<int> { 3840, 2400 },   // 16:10 (WQUXGA)
+            new List<int> { 5120, 2880 },   // 16:9 (5K)
+            new List<int> { 5120, 3200 },   // 16:10
+            new List<int> { 7680, 4320 }    // 16:9 (8K UHD)
+        };
+        private static ConfigEntry<int> customResolution_width;
+        private static ConfigEntry<int> customResolution_height;
+        private static bool custom_flag = false;
+        private void Start() {
+            customResolution_width = Config.Bind<int>("自定义分辨率 Custom Resolution", "宽度 width", 0,"自定义分辨率的宽度，0为取消");
+            customResolution_height = Config.Bind<int>("自定义分辨率 Custom Resolution", "高度 height", 0, "自定义分辨率的高度，0为取消");
+            Mainload.AllFenBData = resolutions;
+            if (customResolution_width.Value != 0 && customResolution_height.Value != 0) {
+                custom_flag = true;
+                Mainload.AllFenBData.Insert(0, new List<int> { customResolution_width.Value, customResolution_height.Value });
+            }
+            Harmony.CreateAndPatchAll(typeof(MoreResolution));
         }
         [HarmonyPostfix]
-        [HarmonyPatch(typeof(SetPanel),"Awake")]]
+        [HarmonyPatch(typeof(SetPanel), "Awake")]
         public static void CreateFenB(SetPanel __instance) {
-            __instance.transform.Find("FenB").Find("AllClass").GetComponent<Dropdown>().options = new List<Dropdown.OptionData>();
-            for (int i = 0; i < Mainload.AllFenBData.Count; i++) {
-                Dropdown.OptionData optionData = new Dropdown.OptionData();
-                optionData.text = 
-                __instance.transform.Find("FenB").Find("AllClass").GetComponent<Dropdown>().options.Add(optionData);
-                //if (__instance.isChangeLanguage) {
-                //    __instance.transform.Find("LoadSpeed").Find("AllClass").GetComponent<Dropdown>().value = -1;
-                //}
+            var options = new List<Dropdown.OptionData>();
+            if (custom_flag) {
+                Dropdown.OptionData optionData = new Dropdown.OptionData {
+                    text = string.Concat(new List<string> {
+                        "* ",
+                        customResolution_width.Value.ToString(),
+                        "x",
+                        customResolution_height.Value.ToString()
+                    })
+                };
             }
+            for (int i = custom_flag?1:0; i < Mainload.AllFenBData.Count; i++) {
+                if (Mainload.AllFenBData[i][0] > Screen.currentResolution.width) {
+                    break;
+                } else if (Mainload.AllFenBData[i][0] == Screen.currentResolution.width && Mainload.AllFenBData[i][1] > Screen.currentResolution.height) {
+                    break;
+                }
+                Dropdown.OptionData optionData = new Dropdown.OptionData {
+                    text = string.Join("x", Mainload.AllFenBData[i])
+                };
+                options.Add(optionData);
+            }
+            __instance.transform.Find("FenB").Find("AllClass").GetComponent<Dropdown>().options = options;
         }
 
     }
