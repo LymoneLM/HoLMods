d293f2578e9833fe1b9d3025e2d01d9c09c1a3aa
LymoneLM <chengziorange233@qq.com>
2025-09-28 23:29:54 +0800

    feat(NIT): 新增测试模组

---
+++ NewItemTest/src/NewItemTest.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+
+namespace NewItemTest
+{
+    [BepInPlugin(MODGUID, MODNAME, VERSION)]
+    public class NewItemTest
+    {
+        public const string MODNAME = "NewItemTest";
+        public const string MODGUID = "cc.lymone.HoL." + MODNAME;
+        public const string VERSION = "1.0.0";
+
+        private static void Start()
+        {
+
+        }
+    }
+}
