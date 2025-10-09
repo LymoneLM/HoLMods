5f1536fa858c98f0d9b2dc57700d2f74c42b9d3d
LymoneLM <chengziorange233@qq.com>
2025-10-10 00:25:47 +0800

    feat(MB): 祠堂容量倍率

---
+++ MemorialBiography/FormulaDataPatch.cs
@@ -0,0 +1,11 @@
+﻿using HarmonyLib;
+
+namespace MemorialBiography {
+    internal class FormulaDataPatch {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(FormulaData), "PaiNumForCiTang")]
+        public static void MultiOfCapacity(ref int __result) {
+            __result *= MemorialBiography.CiTangCapacityMultiplier.Value;
+        }
+    }
+}
--- MemorialBiography/MemorialBiography.cs
+++ MemorialBiography/MemorialBiography.cs
@@ -5,8 +5,14 @@ using HarmonyLib;
 namespace MemorialBiography {
     [BepInPlugin("cc.lymone.HoL.MemorialBiography", "MemorialBiography", "1.0.0")]
     public class MemorialBiography : BaseUnityPlugin {
+        public static ConfigEntry<int> CiTangCapacityMultiplier;
         private void Awake() {
+            CiTangCapacityMultiplier = Config.Bind<int>("配置 Config",
+                "祠堂容量倍率 Shrine Capacity Multiplier", 1);
             Harmony.CreateAndPatchAll(typeof(CiTangPanelPatch));
+            if (CiTangCapacityMultiplier.Value != 1) {
+                Harmony.CreateAndPatchAll(typeof(FormulaDataPatch));
+            }
         }
     }
 }
