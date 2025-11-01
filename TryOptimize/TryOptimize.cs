908d9b6c704ce3da89934dd576c7677622984a46
LymoneLM <chengziorange233@qq.com>
2025-11-01 15:21:55 +0800

    feat(TO): 尝试进行建筑优化

---
+++ TryOptimize/TryOptimize.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace TryOptimize;
+
+[BepInPlugin(MODGUID, MODNAME, VERSION)]
+public class TryOptimize : BaseUnityPlugin
+{
+    public const string MODNAME = "TryOptimize";
+    public const string MODGUID = "cc.lymone.HoL." +  MODNAME;
+    public const string VERSION = "1.0.0";
+
+    internal new static ManualLogSource Logger;
+    
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Harmony.CreateAndPatchAll(typeof(TryOptimize), MODGUID);
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(PerBuildShow), "UpdateShow")]
+    public static bool PerBuildShowCdPatch()
+    {
+        return false;
+    }
+}
