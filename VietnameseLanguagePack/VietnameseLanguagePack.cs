1424214f75ff37900e7fb5f6a83ee7c35a36ef3a
LymoneLM <Lymone@foxmail.com>
2026-02-28 01:03:52 +0800

    feat(VLP): 越南语语言包模组

---
+++ VietnameseLanguagePack/VietnameseLanguagePack.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Reflection;
+using YuanAPI;
+using BepInEx;
+
+namespace VietnameseLanguagePack;
+
+[BepInDependency(YuanAPIPlugin.MODGUID, YuanAPIPlugin.VERSION)]
+[BepInPlugin(MODGUID, MODNAME, VERSION)]
+public class VietnameseLanguagePackPlugin : BaseUnityPlugin
+{
+    private const string MODNAME = "VietnameseLanguagePack";
+    private const string MODGUID = "cc.lymone.HoL." + MODNAME;
+    private const string VERSION = "1.0.0";
+
+    private void Awake()
+    {
+        Localization.RegisterLocale("vi-VN", "Tiếng Việt", ["en-US"]);
+        
+        var executingAssembly = Assembly.GetExecutingAssembly();
+        var modPath = Path.GetDirectoryName(executingAssembly.Location);
+        Localization.LoadFromPath(modPath);
+    }
+}
