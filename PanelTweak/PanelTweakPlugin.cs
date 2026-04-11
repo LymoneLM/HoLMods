73dbdd50d0411646124ac275aeecb42945fa411a
LymoneLM <Lymone@foxmail.com>
2026-04-11 23:56:54 +0800

    feat(PT): 新增界面增强模组

---
+++ PanelTweak/PanelTweakPlugin.cs
@@ -0,0 +1,12 @@
+﻿using BepInEx;
+
+namespace PanelTweak;
+
+[BepInPlugin(MODGUID, MODNAME, VERSION)]
+public class PanelTweakPlugin : BaseUnityPlugin
+{
+    public const string MODNAME = "PanelTweak";
+    public const string MODGUID = "cc.lymone.HoL." + MODNAME;
+    public const string VERSION = "1.0.0";
+    
+}
