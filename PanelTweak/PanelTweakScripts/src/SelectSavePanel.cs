daa7459005ca43c332eb8143ac1f106b3950af7d
LymoneLM <Lymone@foxmail.com>
2026-04-22 19:01:55 +0800

    feat(PT): 新增存档页脚本和简单对象池

---
+++ PanelTweak/PanelTweakScripts/src/SelectSavePanel.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PanelTweak;
+
+public class SelectSavePanel : MonoBehaviour
+{
+    internal static SelectSavePanel Instance { get; private set; }
+    internal static bool LegacyMode = false;
+        
+    private GameObject _loadPanel;
+    private GameObject _startGameUI;
+    private GameObject _initGameUI;
+
+    private Text _title;
+    private GameObject _delCunDPanel;
+
+    private Transform _saveContext;
+    private List<SaveBTEx> _saveList = new List<SaveBTEx>();
+    private SimplePool<SaveBTEx> _savePool;
+        
+    private void Awake()
+    {
+        Instance = this;
+        _loadPanel = transform.parent.Find("LoadPanel").gameObject;
+        _startGameUI = transform.parent.Find("StartGameUI").gameObject;
+        _initGameUI = transform.parent.Find("InitGameUI").gameObject;
+
+        _title = transform.Find("Title").GetComponent<Text>();
+        _delCunDPanel = transform.Find("DelCunDPanel").gameObject;
+            
+        var prefab = Resources.Load<GameObject>("PanelTweak/SaveCell").GetComponent<SaveBTEx>();
+        _savePool = new SimplePool<SaveBTEx>(prefab);
+            
+        _saveContext = transform.Find("ScrollView/ViewPort/Content");
+        for(var i = _saveContext.childCount; i > 0; --i)
+            Destroy(_saveContext.GetChild(i-1).gameObject);
+            
+        transform.Find("CloseBT").GetComponent<Button>().onClick.AddListener(CloseBT);
+        transform.Find("Back").GetComponent<Button>().onClick.AddListener(CloseBT);
+    }
+
+    private void OnEnable()
+    {
+        ReloadSaveList();
+        _delCunDPanel.SetActive(false);
+            
+        _title.text = AllText.Text_UIA[27][Mainload.SetData[4]];
+        transform.localPosition = new Vector3(0f, 500f, 0f);
+        transform.DOLocalMoveY(0f, 0.3f).SetEase(Ease.OutBack, 1f);
+    }
+
+    private void ReloadSaveList()
+    {
+        var count = _saveList.Count;
+        for (var i = count; i > 0; --i)
+        {
+            _savePool.Return(_saveList[i-1]);
+            _saveList.RemoveAt(i-1);
+        }
+            
+        if (LegacyMode)
+        {
+            for (var i = 0; i < 6; i++)
+            {
+                var saveBT = _savePool.Get();
+                saveBT.transform.SetParent(_saveContext);
+                saveBT.SetSave($"FW/{i}");
+                _saveList.Add(saveBT);
+            }
+        }
+        else
+        {
+            var saves = ES3.GetDirectories("FW/");
+            Array.Sort(saves);
+            foreach (var savePath in saves)
+            {
+                var saveBT = _savePool.Get();
+                saveBT.transform.SetParent(_saveContext);
+                saveBT.SetSave($"FW/{savePath}");
+                _saveList.Add(saveBT);
+            }
+        }
+    }
+
+    internal void OpenSave(string savePath, bool haveSave)
+    {
+        Mainload.CunDangIndex_now = savePath;
+        if (haveSave)
+        {
+            Mainload.Guide_order = 10000;
+
+            SaveData.ReadGameData();
+            InitRunData();
+            _loadPanel.GetComponent<LoadPanel>().ShowID = 0;
+            _loadPanel.SetActive(true);
+            gameObject.SetActive(false);
+            return;
+        }
+
+        Mainload.Guide_order = Mainload.Guide_order == 10000 ? 10000 : 0;
+
+        if (Mainload.isFirstGame)
+        {
+            Mainload.SceneID = "M|0";
+            _loadPanel.GetComponent<LoadPanel>().ShowID = 1;
+            _loadPanel.SetActive(true);
+            DefaultSaveData.Load(this);
+            _loadPanel.GetComponent<LoadPanel>().SwitchScene();
+        }
+        else
+        {
+            _initGameUI.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+
+    internal void RemoveSave(string savePath)
+    {
+        Mainload.CunDangIndex_now = savePath;
+        _delCunDPanel.SetActive(true);
+    }
+        
+    private static void InitRunData()
+    {
+        for (var i = 0; i < Mainload.NongZ_now.Count; i++)
+        {
+            for (var j = 0; j < Mainload.NongZ_now[i].Count; j++)
+            {
+                if (Mainload.NongZ_now[i][j][0] == "-1")
+                {
+                    Mainload.NongzHaveData.Add(i + "|" + j);
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(Mainload.FastKey[0]) && !_loadPanel.activeSelf)
+        {
+            CloseBT();
+        }
+    }
+
+    private void CloseBT()
+    {
+        _startGameUI.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
---
+++ PanelTweak/PanelTweakScripts/src/SimplePool.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PanelTweak;
+
+public class SimplePool<T> where T : MonoBehaviour
+{
+    private T _prefab;
+    private Transform _poolRoot;
+        
+    private Queue<T> _pool = new Queue<T>();
+        
+    public SimplePool(T prefab)
+    {
+        _prefab = prefab;
+        _poolRoot = new GameObject("SimplePoolRoot").transform;
+        Object.DontDestroyOnLoad(_poolRoot);
+    }
+
+    public T Get()
+    {
+        var obj = _pool.Count > 0 ? _pool.Dequeue() : Object.Instantiate(_prefab, _poolRoot);
+        obj.gameObject.SetActive(true);
+        return obj;
+    }
+
+    public void Return(T obj)
+    {
+        obj.transform.SetParent(_poolRoot);
+        obj.gameObject.SetActive(false);
+        _pool.Enqueue(obj);
+    }
+        
+    ~SimplePool()
+    {
+        Object.Destroy(_poolRoot);
+    }
+}
