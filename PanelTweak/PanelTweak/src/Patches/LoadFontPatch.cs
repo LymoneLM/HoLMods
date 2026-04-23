a2c17006d56fb1bd6a25a6b93dc9a9b74749d028
LymoneLM <Lymone@foxmail.com>
2026-04-24 04:39:52 +0800

    feat(PT): 新增字体获取补丁

---
+++ PanelTweak/PanelTweak/src/Patches/LoadFontPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using UnityEngine.UI;
+
+namespace PanelTweak;
+
+[HarmonyPatch]
+public class LoadFontPatch {
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(StartGameUI), "Start")]
+    private static void LoadFont(StartGameUI __instance)
+    {
+        var ts = __instance.transform.parent.Find("CunDangUI").Find("DelCunDPanel");
+        var font = ts.Find("TipA").GetComponent<Text>().font;
+        FontManager.BoldFont = font;
+        font = ts.Find("CancelBT").Find("Text").GetComponent<Text>().font;
+        FontManager.MediumFont = font;
+    }
+}
--- PanelTweak/PanelTweakScripts/src/FontManager.cs
+++ PanelTweak/PanelTweakScripts/src/FontManager.cs
@@ -6,4 +6,11 @@ public static class FontManager
 {
     public static Font BoldFont;
     public static Font MediumFont;
+
+    static FontManager()
+    {
+        var font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+        BoldFont = font;
+        MediumFont = font;
+    }
 }
