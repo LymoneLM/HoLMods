37dd00530cffb33c82fbf342ce31baa3a48ec0cf
LymoneLM <Lymone@foxmail.com>
2026-04-24 04:38:57 +0800

    feat(PT): 新增添加面板的修补接入点

--- PanelTweak/PanelTweak/src/PanelTweakPlugin.cs
+++ PanelTweak/PanelTweak/src/PanelTweakPlugin.cs
@@ -1,4 +1,9 @@
-﻿using BepInEx;
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
 
 namespace PanelTweak;
 
@@ -8,5 +13,18 @@ public class PanelTweakPlugin : BaseUnityPlugin
     public const string MODNAME = "PanelTweak";
     public const string MODGUID = "cc.lymone.HoL." + MODNAME;
     public const string VERSION = "1.0.0";
-    
+
+    internal static Harmony Harmony = new(MODGUID);
+    internal new static ManualLogSource Logger;
+    internal static AssetBundle AssetBundle;
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+        var asm = Assembly.GetExecutingAssembly();
+        Harmony.PatchAll(asm);
+        
+        var path = Path.GetDirectoryName(asm.Location);
+        AssetBundle = AssetBundle.LoadFromFile(path + "/paneltweak");
+    }
 }
---
+++ PanelTweak/PanelTweak/src/Patches/PluginEntryPatch.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace PanelTweak;
+
+[HarmonyPatch]
+public class PluginEntryPatch
+{
+    private static GameObject _selectSavePanel;
+    
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(StartGameUI), "Start")]
+    private static void LoadPanels(StartGameUI __instance)
+    {
+        var allPanel = __instance.transform.parent;
+        _selectSavePanel = Object.Instantiate(
+            PanelTweakPlugin.AssetBundle.LoadAsset<GameObject>("Assets/PanelTweak/SelectSavePanel.prefab"),
+            allPanel, false);
+        _selectSavePanel.SetActive(false);
+        
+        Object.Destroy(allPanel.Find("CunDangUI").gameObject);
+    }
+    
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(StartGameUI), "StartBT")]
+    private static bool StartSelectSavePanelBT(StartGameUI __instance)
+    {
+        _selectSavePanel.SetActive(true);
+        __instance.gameObject.SetActive(false);
+        return false;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(InitGameUI), "CloseBT")]
+    private static bool CloseSelectSavePanelBT(InitGameUI __instance)
+    {
+        _selectSavePanel.SetActive(true);
+        __instance.gameObject.SetActive(false);
+        return false;
+    }
+}
